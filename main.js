abf34795d72e22ad5b3f7b9af812da2229cd3097
floraprvt <f.preuvot@live.fr>
2024-03-16 12:22:40 -0400

    feat(sprite): add sprite class to properly draw resources in canvas

--- main.js
+++ main.js
@@ -1,29 +1,48 @@
 import './style.css'
-import resources from './src/resources'
+import resources from './src/Resources'
+import { Sprite } from './src/Sprite'
+import { Vector2 } from './src/Vector2'
 
 const canvas = document.getElementById('game-canvas')
 const ctx = canvas.getContext('2d')
 
+const skySprite = new Sprite({
+  resource: resources.images.sky,
+  frameSize: new Vector2(320, 180),
+})
+
+const groundSprite = new Sprite({
+  resource: resources.images.ground,
+  frameSize: new Vector2(320, 180),
+})
+
+const hero = new Sprite({
+  resource: resources.images.hero,
+  frameSize: new Vector2(32, 32),
+  hFrames: 3,
+  vFrames: 8,
+  frame: 1,
+})
+const heroPos = new Vector2(16 * 6, 16 * 5)
+
+const shadow = new Sprite({
+  resource: resources.images.shadow,
+  frameSize: new Vector2(32, 32),
+})
+
 const draw = () => {
-  const sky = resources.images.sky
-  if (sky.isLoaded) {
-    ctx.drawImage(sky.image, 0, 0)
-  }
+  skySprite.drawImage(ctx, 0, 0)
+  groundSprite.drawImage(ctx, 0, 0)
 
-  const hero = resources.images.hero
-  if (hero.isLoaded) {
-    ctx.drawImage(hero.image, 0, 0)
-  }
+  const heroOffset = new Vector2(-8, -21)
+  const heroPosX = heroPos.x + heroOffset.x
+  const heroPosY = heroPos.y + heroOffset.y
 
-  const ground = resources.images.ground
-  if (ground.isLoaded) {
-    ctx.drawImage(ground.image, 0, 0)
-  }
-
-  const shadow = resources.images.shadow
-  if (shadow.isLoaded) {
-    ctx.drawImage(shadow.image, 0, 0)
-  }
+  shadow.drawImage(ctx, heroPosX, heroPosY)
+  hero.drawImage(ctx, heroPosX, heroPosY)
 }
 
-setInterval(draw, 300)
+setInterval(() => {
+  hero.frame += 1
+  draw()
+}, 300)

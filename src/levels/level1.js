4497f5c9aa853f1f0f078c6566fe8f88370c0ab7
floraprvt <f.preuvot@live.fr>
2024-03-18 17:25:33 -0400

    feat(level1): add missing walls

--- src/levels/level1.js
+++ src/levels/level1.js
@@ -1,7 +1,13 @@
 export const walls = new Set()
 
-/** Tree */
+/** Trees */
 walls.add('64,48')
+
+walls.add('208,64')
+walls.add('224,32')
+
+/** House */
+walls.add('224,64')
 
 /** Squares */
 walls.add('64,64')
@@ -9,8 +15,60 @@ walls.add('64,80')
 walls.add('80,64')
 walls.add('80,80')
 
+walls.add('128,48')
+walls.add('144,48')
+
 /** Water */
 walls.add('112,80')
 walls.add('128,80')
 walls.add('144,80')
 walls.add('160,80')
+
+/** Rocks */
+walls.add('192,96')
+walls.add('208,96')
+walls.add('224,96')
+
+/** Borders Top */
+walls.add('48,32')
+walls.add('64,32')
+walls.add('80,32')
+walls.add('96,32')
+
+walls.add('112,16')
+walls.add('128,16')
+walls.add('144,16')
+walls.add('160,16')
+walls.add('176,16')
+walls.add('192,16')
+walls.add('208,16')
+walls.add('224,16')
+
+walls.add('240,32')
+
+/** Borders Right */
+walls.add('256,48')
+walls.add('256,64')
+walls.add('256,80')
+walls.add('256,96')
+
+/** Borders Bottom */
+walls.add('48,112')
+walls.add('64,112')
+walls.add('80,112')
+walls.add('96,112')
+walls.add('112,112')
+walls.add('128,112')
+walls.add('144,112')
+walls.add('160,112')
+walls.add('176,112')
+walls.add('192,112')
+walls.add('208,112')
+walls.add('224,112')
+walls.add('240,112')
+
+/** Borders Left */
+walls.add('32,48')
+walls.add('32,64')
+walls.add('32,80')
+walls.add('32,96')
